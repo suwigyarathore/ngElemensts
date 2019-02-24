fb085f21be04bd9760116068c14686395c266ff8
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-24 11:14:38 +0100

    Including input and output events

--- src/app/greet/greet.component.ts
+++ src/app/greet/greet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 
 @Component({
   // selector: 'app-greet',
@@ -6,7 +6,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./greet.component.css"]
 })
 export class GreetComponent implements OnInit {
+  @Input() name: string;
+  @Output() greet = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
+
+  emitGreet(name) {
+    this.greet.emit(`Hi, ${name}`);
+  }
 }
